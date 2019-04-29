dc5417180868873862e0cd24e202d55d009446fb
SIRQWA <ger619>
2019-04-29 10:43:37 +0300

    user index

--- lib/remit/profile.ex
+++ lib/remit/profile.ex
@@ -1,6 +1,9 @@
 defmodule Remit.Profile do
   use Ecto.Schema
+
   import Ecto.Changeset
+
+  alias Remit.Repo
 
   schema "profile" do
     field :name, :string
@@ -17,6 +20,7 @@ defmodule Remit.Profile do
     |> cast(attrs, [:name, :type, :currency])
     |> validate_required([:name, :type, :currency])
     |> process_slug
+    |> unique_constraint(:name, name: "profiles_slug_index")
   end
 
   # Slug check on name
@@ -27,5 +31,18 @@ defmodule Remit.Profile do
     else
       changeset
     end
+  end
+
+  def create(params) do
+    %__MODULE__{}
+    |> changeset(params)
+    |> put_change(:type, "business")
+    |> Repo.insert()
+  end
+
+  def update_profile(%__MODULE__{} = profile, params) do
+    profile
+    |> changeset(params)
+    |> Repo.update()
   end
 end
--- lib/remit/repo.ex
+++ lib/remit/repo.ex
@@ -2,6 +2,5 @@ defmodule Remit.Repo do
   use Ecto.Repo,
     otp_app: :remit,
     adapter: Ecto.Adapters.Postgres
-
-  use Scrivener, page_size: 10
+    use Scrivener, page_size: 2
 end
