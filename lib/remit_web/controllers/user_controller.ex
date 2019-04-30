1557e21b2e549ba176a8a764f4f25ac09c1609ce
Nicholas <nicholaschikas18@gmail.com>
2019-04-30 16:20:11 +0300

    update edit and show function

--- lib/remit_web/controllers/user_controller.ex
+++ lib/remit_web/controllers/user_controller.ex
@@ -5,6 +5,7 @@ defmodule RemitWeb.UserController do
   alias Remit.User
   alias Remit.Accounts
 
+
   def index(conn, params) do
     page =
       case params["query"] do
@@ -36,37 +37,30 @@ defmodule RemitWeb.UserController do
     end
   end
 
-  # def show(conn, %{"id" => id}) do
-  #   user = User.get_user!(id)
-  #   render(conn, "show.html", user: user)
-  # end
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    render(conn, "show.html", user: user)
+  end
   #
-  # def edit(conn, %{"id" => id}) do
-  #   user = User.get_user!(id)
-  #   changeset = User.change_user(user)
-  #   render(conn, "edit.html", user: user, changeset: changeset)
-  # end
-  #
-  # def update(conn, %{"id" => id, "user" => user_params}) do
-  #   user = User.get_user!(id)
-  #
-  #   case User.update_user(user, user_params) do
-  #     {:ok, user} ->
-  #       conn
-  #       |> put_flash(:info, "User updated successfully.")
-  #       |> redirect(to: Routes.user_path(conn, :show, user))
-  #
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       render(conn, "edit.html", user: user, changeset: changeset)
-  #   end
-  # end
-  #
-  # def delete(conn, %{"id" => id}) do
-  #   user = User.get_user!(id)
-  #   {:ok, _user} = User.delete_user(user)
-  #
-  #   conn
-  #   |> put_flash(:info, "User deleted successfully.")
-  #   |> redirect(to: Routes.user_path(conn, :index))
-  # end
+   def edit(conn, %{"id" => id}) do
+     user = Accounts.get_user!(id)
+     changeset = Accounts.change_user(user)
+     render(conn, "edit.html", user: user, changeset: changeset)
+   end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+
+    case Accounts.update_user(user, user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
+    end
+  end
+
+
 end
