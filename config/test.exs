bb769f07aa0f7d2615a12d238bd2ea24f20fa72b
Larry Weya <larryweya@gmail.com>
2019-05-10 16:37:20 +0300

    Add bcrypt test config to make hashing faster in test

--- config/test.exs
+++ config/test.exs
@@ -17,6 +17,6 @@ config :remit, Remit.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-config(:argon2_elixir)
+config :bcrypt_elixir, log_rounds: 4
 
 import_config "dev.local.exs"
