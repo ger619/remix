3c14d836fa2a279ba9d2c36a754ee39028f0c2ec
David Ger <draker74@gmail.com>
2019-06-10 11:33:49 +0300

    Code clean up

--- lib/remit_web/controllers/password_controller.ex
+++ lib/remit_web/controllers/password_controller.ex
@@ -12,13 +12,13 @@ defmodule RemitWeb.PasswordController do
     user = conn.assigns.current_user.id |> Accounts.get_user!()
 
     case PasswordChange.update_password(params, user) do
-      {:error, changeset} ->
-        render(conn, "password_change.html", changeset: changeset)
-
       {:ok, _} ->
         conn
         |> put_flash(:info, "Your password has been updated.")
         |> redirect(to: Routes.page_path(conn, :dashboard))
+
+      {:error, changeset} ->
+        render(conn, "password_change.html", changeset: changeset)
     end
   end
 end
