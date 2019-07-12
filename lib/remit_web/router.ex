2225d7db6c93d66a32198d654af0d11984617645
Kingsella <jmusa945@gmail.com>
2019-07-12 11:56:56 +0300

    added POST for the search functionally

--- lib/remit_web/router.ex
+++ lib/remit_web/router.ex
@@ -46,6 +46,7 @@ defmodule RemitWeb.Router do
     pipe_through [:browser, :authenticated, :requires_password_change]
 
     get "/dashboard", PageController, :dashboard
+    post "/dashboard", PageController, :dashboard
     resources "/profiles", ProfileController, except: [:delete]
 
     resources "/users", UserController
